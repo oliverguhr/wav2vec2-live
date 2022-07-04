a22f099010820776cf179a3d142411243b392d94
Oliver Guhr <oliver.guhr@posteo.de>
2022-07-04 14:18:18 +0200

    updated config

--- onnx_performance_evaluation.py
+++ onnx_performance_evaluation.py
@@ -1,18 +1,24 @@
 import timeit
 import soundfile as sf
+import torch
 from wav2vec2_inference import Wave2Vec2Inference
 from wav2vec2_onnx_inference import Wave2Vec2ONNXInference
 
 if __name__ == "__main__":
+    torch.set_num_threads(16)
+    
     audio_input, samplerate = sf.read("test.wav")
     print("Model test")
-    asr_onnx_opt = Wave2Vec2ONNXInference("jonatasgrosman/wav2vec2-large-xlsr-53-german","wav2vec2-large-xlsr-53-german-opt.onnx")
-    asr_onnx_quant = Wave2Vec2ONNXInference("jonatasgrosman/wav2vec2-large-xlsr-53-german","wav2vec2-large-xlsr-53-german.quant.onnx")
-    asr_onnx = Wave2Vec2ONNXInference("jonatasgrosman/wav2vec2-large-xlsr-53-german","wav2vec2-large-xlsr-53-german.onnx")
+
+    base_model = "oliverguhr/wav2vec2-large-xlsr-53-german-cv9"
+
+    asr_onnx_opt = Wave2Vec2ONNXInference(base_model,"wav2vec2-large-xlsr-53-german-cv9-opt.onnx")
+    asr_onnx_quant = Wave2Vec2ONNXInference(base_model,"wav2vec2-large-xlsr-53-german-cv9.quant.onnx")
+    asr_onnx = Wave2Vec2ONNXInference(base_model,"wav2vec2-large-xlsr-53-german-cv9.onnx")
     text = asr_onnx.buffer_to_text(audio_input)
     print(text)
 
-    asr = Wave2Vec2Inference("jonatasgrosman/wav2vec2-large-xlsr-53-german")
+    asr = Wave2Vec2Inference(base_model)
     text = asr.buffer_to_text(audio_input)
     print(text)
 
