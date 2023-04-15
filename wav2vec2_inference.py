854cb1d3f36efa4c5833a0b32ca048c4c38a6bd6
Programador Artificial <programador.artificial@gmail.com>
2023-04-15 12:12:57 -0300

    Add GPU inference

--- wav2vec2_inference.py
+++ wav2vec2_inference.py
@@ -3,17 +3,18 @@ import torch
 from transformers import AutoModelForCTC, AutoProcessor, Wav2Vec2Processor, pipeline
 
 # Improvements: 
-# - gpu / cpu flag
 # - convert non 16 khz sample rates
 # - inference time log
 
 class Wave2Vec2Inference():
-    def __init__(self,model_name, hotwords=[], use_lm_if_possible = True):
+    def __init__(self,model_name, hotwords=[], use_lm_if_possible = True, use_gpu=True):
+        self.device = "cuda" if use_gpu and torch.cuda.is_available() else "cpu"
         if use_lm_if_possible:            
             self.processor = AutoProcessor.from_pretrained(model_name)
         else:
             self.processor = Wav2Vec2Processor.from_pretrained(model_name)
         self.model = AutoModelForCTC.from_pretrained(model_name)
+        self.model.to(self.device)
         self.hotwords = hotwords
         self.use_lm_if_possible = use_lm_if_possible
 
@@ -24,7 +25,8 @@ class Wave2Vec2Inference():
         inputs = self.processor(torch.tensor(audio_buffer), sampling_rate=16_000, return_tensors="pt", padding=True)
 
         with torch.no_grad():
-            logits = self.model(inputs.input_values, attention_mask=inputs.attention_mask).logits            
+            logits = self.model(inputs.input_values.to(self.device),
+                                attention_mask=inputs.attention_mask.to(self.device)).logits            
 
         if hasattr(self.processor, 'decoder') and self.use_lm_if_possible:
             transcription = \
